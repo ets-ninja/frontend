d8bcdd8a41a769345c82137ec04c8912c0c8c8c5
Pavlyuk Bohdan <b.i.pavlyuk@gmail.com>
2022-09-27 22:13:11 +0300

    Intro page/ checking

--- src/pages/IntoPage/IntroPage.jsx
+++ src/pages/IntoPage/IntroPage.jsx
@@ -5,11 +5,21 @@ import IntroTitle from './IntroTitle'
 
 const IntroPage = () => {
     return (
-        <div style={{paddingTop: '10%'}}>
-            {window.innerWidth > 900 ? <div style={{marginTop: '-10%'}}><IntroTitle /></div> : <></> }
-                <IntroSwiper/>
-        </div>
-    )
+      <div style={{ paddingTop: '10%' }}>
+
+        {window.innerWidth > 900 ? (
+          <div style={{ marginTop: '-10%' }}>
+            <IntroTitle />
+          </div>
+        ) : (
+          <></>
+        )}
+
+        {/* must be more content */}
+        
+        <IntroSwiper />
+      </div>
+    );
 }
 
 export default IntroPage
