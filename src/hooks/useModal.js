29f798e24a331fc5f6da55dfe6801206339ba09d
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-09-30 20:27:03 +0300

    Creates custom hook for modal

---
+++ src/hooks/useModal.js
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import modalSlice from '../redux/modal/modalSlice';
+
+export default function useModal() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const open = (path, data = null) => {
+    navigate(`/modal/${path}`, {
+      state: { backgroundLocation: location },
+    });
+    dispatch(
+      modalSlice.actions.setModalData({
+        path,
+        data,
+      }),
+    );
+  };
+
+  const close = () => {
+    navigate(location.state === null ? '/' : -1);
+  };
+
+  return { open, close };
+}
