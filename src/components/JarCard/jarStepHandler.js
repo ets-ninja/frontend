96f48f04f07a869b2a49d092bc7b9b0e4b0ce453
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-10-06 21:37:05 +0300

    Renames jarImage folder

--- src/components/JarCard/jarStepHandler.js
+++ src/components/JarCard/jarStepHandler.js
@@ -1,9 +1,9 @@
-import JarStepOne from './jarImages/JarStep1.png';
-import JarStepTwo from './jarImages/JarStep2.png';
-import JarStepThree from './jarImages/JarStep3.png';
-import JarStepFour from './jarImages/JarStep4.png';
-import JarStepFive from './jarImages/JarStep5.png';
-import JarStepSix from './jarImages/JarStep6.png';
+import JarStepOne from './images/JarStep1.png';
+import JarStepTwo from './images/JarStep2.png';
+import JarStepThree from './images/JarStep3.png';
+import JarStepFour from './images/JarStep4.png';
+import JarStepFive from './images/JarStep5.png';
+import JarStepSix from './images/JarStep6.png';
 
 const jarStepHandler = (value, goal) => {
   const JarImagesArr = [
