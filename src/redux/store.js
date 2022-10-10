c5f12f6b527149599bbe5d37b131ea9f907cbe55
ShevtsovVolodymyr <61378255+ShevtsovVolodymyr@users.noreply.github.com>
2022-10-10 12:45:10 +0300

    Updated auth flow (#25)
    * Updated auth flow
    
    * Fixed bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createListenerMiddleware } from '@reduxjs/toolkit';
 import {
   persistReducer,
   persistStore,
@@ -8,18 +8,41 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
-} from 'redux-persist';
+} from 'reduxjs-toolkit-persist';
+import authPersistConfig from './auth/authPersistConfig';
 
+// Reducers
+import authReducer from './auth/authSlice';
 import userReducer from './user/userSlice';
 import requestReducer from './request/requestSlice';
-import userConfig from './user/userConfig';
-
 import modalSlice from './modal/modalSlice';
 import modalConfig from './modal/modalConfig';
 
+// Actions
+import { refresh } from './auth/authActions';
+
+const listenerMiddleware = createListenerMiddleware();
+
+listenerMiddleware.startListening({
+  type: 'persist/REHYDRATE',
+  effect: async (action, listenerApi) => {
+    if (!action.payload && action.key === 'persistedAuth') {
+      listenerApi.dispatch(refresh());
+    } else if (
+      action.payload.token === null &&
+      action.key === 'persistedAuth'
+    ) {
+      listenerApi.dispatch(refresh());
+    } else {
+      listenerApi.cancelActiveListeners();
+    }
+  },
+});
+
 export const store = configureStore({
   reducer: {
-    user: persistReducer(userConfig, userReducer),
+    auth: persistReducer(authPersistConfig, authReducer),
+    user: userReducer,
     modal: persistReducer(modalConfig, modalSlice.reducer),
     request: requestReducer,
   },
@@ -28,7 +51,7 @@ export const store = configureStore({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }),
+    }).prepend(listenerMiddleware.middleware),
 });
 
 export const persistor = persistStore(store);
