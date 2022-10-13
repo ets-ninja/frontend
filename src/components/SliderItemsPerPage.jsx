db74be38d2e278f35e1a663f1a94b8b5c4886498
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-10-13 15:27:43 +0300

    Creates slider for items per page

---
+++ src/components/SliderItemsPerPage.jsx
@@ -0,0 +1,29 @@
+import { Slider } from '@mui/material';
+import { useMediaQuery } from 'react-responsive';
+import { useState } from 'react';
+
+export default function SliderItmesPerPage({ setJarsPerPage }) {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1200 });
+  const [currentPerPage, setCurrentPerPage] = useState(() =>
+    isTablet ? 10 : 9,
+  );
+  return (
+    <Slider
+      value={currentPerPage}
+      step={isTablet ? 2 : 3}
+      min={isTablet ? 2 : 3}
+      max={isTablet ? 40 : 45}
+      aria-label="jars per page"
+      valueLabelFormat={() => 'jars per page'}
+      valueLabelDisplay="auto"
+      marks={[{ value: currentPerPage, label: currentPerPage }]}
+      onChange={(_, value) => {
+        setCurrentPerPage(value);
+      }}
+      onChangeCommitted={(_, value) => {
+        setJarsPerPage(value);
+      }}
+      sx={{ maxWidth: '320px' }}
+    />
+  );
+}
