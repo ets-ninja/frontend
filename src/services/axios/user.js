b519e8391912fb881bbceb43a2b2a4fef9d77ade
v.shevtsov <versaptr@gmail.com>
2022-10-17 18:04:50 +0300

    Refactored axios interceptors, removed comments

--- src/services/axios/user.js
+++ src/services/axios/user.js
@@ -21,12 +21,9 @@ instance.interceptors.response.use(
   response => {
     return response;
   },
-  // error => {
-  //   if (error.response.status === 401) {
-  //     store.dispatch(logout());
-  //   }
-  //   return Promise.reject(error);
-  // },
+  error => {
+    return Promise.reject(error);
+  },
 );
 
 export default instance;
